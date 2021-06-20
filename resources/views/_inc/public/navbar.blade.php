1943850593768ce8db927df3cb63b7f4533c5196
gsbarbo <gagebarbour96@gmail.com>
2021-06-20 06:49:03 -0500

    fixed bugs

--- resources/views/_inc/public/navbar.blade.php
+++ resources/views/_inc/public/navbar.blade.php
@@ -1,4 +1,5 @@
-<header id="top" class="fixed flex flex-col w-full bg-primary lg:relative pin-t pin-r pin-l">
+<header id="top" class="fixed z-50 flex flex-col w-full bg-primary lg:relative pin-t pin-r pin-l"
+    x-data="{ mobileMenuOpen : false }">
     <nav id="site-menu"
         class="flex flex-col items-center justify-between w-full px-4 py-1 mx-auto shadow lg:w-4/5 bg-primary lg:flex-row lg:px-6 lg:shadow-none">
         <div
@@ -7,26 +8,27 @@
                 Gage's Space
             </a>
             <button id="menuBtn" class="block hamburger lg:hidden focus:outline-none" type="button"
-                onclick="navToggle();">
+                @click="mobileMenuOpen = !mobileMenuOpen">
                 <span class="bg-white hamburger__top-bun"></span>
                 <span class="bg-white hamburger__bottom-bun"></span>
             </button>
         </div>
-        <div id="menu"
+        <div id="menu" @click.away="mobileMenuOpen = false"
+            :class="{ 'flex' : mobileMenuOpen , 'hidden' : !mobileMenuOpen}"
             class="flex-col items-center self-end hidden w-full h-full py-1 pb-4 pl-10 lg:w-auto lg:self-center lg:flex lg:flex-row lg:py-0 lg:pb-0">
-            <a href="{{ route('home') }}"
+            <a href="{{ route('home') }}" @click="mobileMenuOpen = false"
                 class="block w-full py-2 font-semibold hover:underline lg:w-auto lg:px-4 lg:py-1 lg:pt-2">Home</a>
-            <a href="#top"
+            <a href="#top" @click="mobileMenuOpen = false"
                 class="block w-full py-2 font-semibold hover:underline lg:w-auto lg:px-4 lg:py-1 lg:pt-2">About</a>
-            <a href="#skills"
+            <a href="#skills" @click="mobileMenuOpen = false"
                 class="block w-full py-2 font-semibold hover:underline lg:w-auto lg:px-4 lg:py-1 lg:pt-2">Skills</a>
-            <a href="#projects"
+            <a href="#projects" @click="mobileMenuOpen = false"
                 class="block w-full py-2 font-semibold hover:underline lg:w-auto lg:px-4 lg:py-1 lg:pt-2">Projects</a>
-            <a href="#contact"
+            <a href="#contact" @click="mobileMenuOpen = false"
                 class="block w-full py-2 font-semibold hover:underline lg:w-auto lg:px-4 lg:py-1 lg:pt-2">Contact</a>
-            <a href="#"
+            <a href="#" @click="mobileMenuOpen = false"
                 class="block w-full py-2 font-semibold hover:underline lg:w-auto lg:px-4 lg:py-1 lg:pt-2">Blog</a>
-            <a href="#"
+            <a href="#" @click="mobileMenuOpen = false"
                 class="block w-full py-2 font-semibold hover:underline lg:w-auto lg:px-4 lg:py-1 lg:pt-2">Docs</a>
         </div>
     </nav>
