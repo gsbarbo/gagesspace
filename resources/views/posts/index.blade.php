e8a76aae41b4547e2dee5ab73eb9f52d58161294
gsbarbo <gagebarbour96@gmail.com>
2021-06-27 11:21:28 -0500

    Fixed Login Page

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -49,13 +49,16 @@
 
                 <!-- Sidebar Section -->
                 <aside class="flex flex-col w-full px-3 md:w-1/3">
-                    <div class="flex flex-wrap mx-auto">
-                        <a href="{{ route('blog.create') }}" class="px-3 py-2 m-3 bg-indigo-600">
-                            <i class="fas fa-plus"></i> Create Post</a>
-                        <a href="{{ route('blog.index') }}" class="px-3 py-2 m-3 bg-indigo-600">
-                            <i class="fas fa-eye"></i> View Hidden Posts</a>
-                    </div>
+                    @auth
 
+                        <div class="flex flex-wrap mx-auto">
+
+                            <a href="{{ route('blog.create') }}" class="px-3 py-2 m-3 bg-indigo-600">
+                                <i class="fas fa-plus"></i> Create Post</a>
+                            <a href="{{ route('blog.index') }}" class="px-3 py-2 m-3 bg-indigo-600">
+                                <i class="fas fa-eye"></i> View Hidden Posts</a>
+                        </div>
+                    @endauth
                     <div class="flex flex-col w-full p-6 my-4 border-2 border-indigo-600">
                         <p class="pb-5 text-xl font-semibold text-indigo-600 underline">Catagories</p>
                         <a href="{{ route('blog.index') }}" class="pb-2 hover:underline">All Posts</a>
